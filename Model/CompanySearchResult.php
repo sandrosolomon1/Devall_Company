2cb7d6e8d99cbfdfa22968b2369c28551db06457
mishkaie <sandroqoiava7@hotmail.com>
2021-07-16 14:19:45 +0400

    dl

--- Model/CompanySearchResult.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Devall\Company\Model;
-
-use Magento\Framework\Api\SearchResults;
-use Devall\Company\Api\Data\CompanySearchResultInterface;
-
-class CompanySearchResult extends SearchResults implements CompanySearchResultInterface
-{}
